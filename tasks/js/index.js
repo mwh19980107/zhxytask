fcc50272a3db5548507d7c9ccd9a04a76b2b514d
mwh19980107 <31238512+mwh19980107@users.noreply.github.com>
2017-09-20 10:42:50 +0800

    sss

--- tasks/js/index.js
+++ tasks/js/index.js
@@ -1,24 +1,30 @@
-//var killer =document.getElementById("killer");
-//var populace =document.getElementById("populace");
-//var tube1 = document.getElementById("tube");
-//var tubea1 = document.getElementById("tubea");
+
+
+
 var foo = document.getElementById("foo");
 var qua=document.getElementById("qua");
+
+
+
 qua.onchange = function() {
 	var popel = parseInt(document.getElementById("qua").value);
-
+//	var killer =ParseInt(document.getElementById("killer").value);
+//	var populace =parseInt(document.getElementById("populace").value);
+    
 	if(isNaN(popel) || popel < 4 || popel > 18) {
 		alert("请输入正确的玩家数量");
+		document.getElementById("killer").value="";
+		document.getElementById("populace").value="";
 	} else if(popel >= 4 && popel <= 9) {
-		killer.innerHTML = 2;
-		populace.innerHTML = popel - killer.innerHTML;
+		killer.value = "2";
+		populace.value = popel - killer.value;
 	} else if(popel >= 10 && popel <= 15) {
-		killer.innerHTML = 4;
-		populace.innerHTML = popel - killer.innerHTML;
+		killer.value = 4;
+		populace.value = popel - killer.value;
 	} else if(popel >= 16 && popel <= 18) {
-		killer.innerHTML = 5;
-		populace.innerHTML = popel - killer.innerHTML;
-	}
+		killer.value = 5;
+		populace.value = popel - killer.value;
+	} 
 }
 
 var upset = [];
@@ -33,10 +39,10 @@ foo.onclick = function() {
 		window.location.href = "index(3).html"
 //	}
 
-	for(var i = 0; i < killer.innerHTML; i++) {
+	for(var i = 0; i < killer.value; i++) {
 		upset.push('幽灵')
 	}
-	for(var i = 0; i < populace.innerHTML; i++) {
+	for(var i = 0; i < populace.value; i++) {
 		upset.push("水民")
 	}
 	upset.sort(function() {
